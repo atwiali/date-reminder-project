70ac455ed927882221e68010aad73d8996fea3b3
Ali Atwi <aliatwiat5@gmail.com>
2022-12-08 21:55:27 +0200

    image

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 export const person = [
-  { id: "0", name: "Ali Atwi", Date: "5 pm ", img: "1.jpg" },
+  { id: "0", name: "Hsen lhsen", Date: "9 pm ", img: "download.jpg" },
   { id: "1", name: "Ali Atwi", Date: "5 pm ", img: "1.jpg" },
   { id: "2", name: "Issa Habhab", Date: "3 pm ", img: "2.jpg" },
   { id: "3", name: "Mhmd Ali ", Date: "8 am ", img: "3.jpg" },
   { id: "4", name: "Jad ghoson ", Date: "10 am ", img: "4.jpg" },
-  { id: "5", name: "Jad ghoson ", Date: "10 am ", img: "5.jpg" },
+  { id: "5", name: "Ousama habhab ", Date: "10 am ", img: "5.jpg" },
 ];
